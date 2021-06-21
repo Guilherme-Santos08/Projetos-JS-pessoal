6192295d00414a50cc6124d4c80b6245e42c1c20
guilherme-santos08 <augusto.gui.2208@gmail.com>
2021-06-21 14:53:52 -0300

    Tudo funcionando

--- 4- border-radius/app.js
+++ 4- border-radius/app.js
@@ -1,14 +1,41 @@
-const topLeft = document.querySelector(".top-left")
-const topRight = document.querySelector(".top-right")
-const bottomLeft = document.querySelector(".bottom-left")
-const bottomRight = document.querySelector(".bottom-right")
+const topLeft = document.querySelector(".top-left");
+const topRight = document.querySelector(".top-right");
+const bottomLeft = document.querySelector(".bottom-left");
+const bottomRight = document.querySelector(".bottom-right");
 
-const showRadius = document.querySelector(".show-radius")
-const viewsRadius = document.querySelector(".view-radius")
-const copy = document.querySelector(".copy")
+const inputs = document.querySelectorAll(".inputs");
+
+const showRadius = document.querySelector(".show-radius");
+const viewsRadius = document.querySelector(".view-radius");
+const copyRadius = document.querySelector(".copy");
 
 // viewsRadius.textContent = topLeft.value
 
 const generateShow = () => {
-   
-}
+  const radiusTl =
+    (showRadius.style.borderTopLeftRadius = `${topLeft.value}px`);
+  const radiusTr =
+    (showRadius.style.borderTopRightRadius = `${topRight.value}px`);
+  const radiusBl =
+    (showRadius.style.borderBottomLeftRadius = `${bottomLeft.value}px`);
+  const radiusBr =
+    (showRadius.style.borderBottomRightRadius = `${bottomRight.value}px`);
+
+  const copy =
+    (viewsRadius.innerHTML = `border-radius: ${radiusTl} ${radiusTr} ${radiusBl} ${radiusBr}`);
+};
+
+copyRadius.addEventListener("click", () => {
+  let textArea = document.createElement("textarea");
+  textArea.value = viewsRadius.textContent;
+  document.body.appendChild(textArea);
+  textArea.select();
+  document.execCommand("Copy");
+  textArea.remove();
+});
+
+inputs.forEach(() => {
+  this.addEventListener("keyup", () => {
+    generateShow();
+  });
+});
