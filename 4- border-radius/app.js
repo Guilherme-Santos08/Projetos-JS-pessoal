2a39e9e3afe3eeb2c55fce0c5e2f788fab0c7be5
guilherme-santos08 <augusto.gui.2208@gmail.com>
2021-06-20 23:47:32 -0300

    criando gerador!

---
+++ 4- border-radius/app.js
@@ -0,0 +1,14 @@
+const topLeft = document.querySelector(".top-left")
+const topRight = document.querySelector(".top-right")
+const bottomLeft = document.querySelector(".bottom-left")
+const bottomRight = document.querySelector(".bottom-right")
+
+const showRadius = document.querySelector(".show-radius")
+const viewsRadius = document.querySelector(".view-radius")
+const copy = document.querySelector(".copy")
+
+// viewsRadius.textContent = topLeft.value
+
+const generateShow = () => {
+   
+}
