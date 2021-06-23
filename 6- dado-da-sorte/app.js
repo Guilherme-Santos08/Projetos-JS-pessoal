af62c22926f861c0b80b4e510377f5dda34078bd
Guilherme Ribeiro <augusto.gui.2208@gmail.com>
2021-06-23 00:21:13 -0300

    o que tenho que fazer

---
+++ 6- dado-da-sorte/app.js
@@ -0,0 +1,15 @@
+const side1 = document.querySelector(".dado__numero--1")
+const side2 = document.querySelector(".dado__numero--2")
+const side3 = document.querySelector(".dado__numero--3")
+const side4 = document.querySelector(".dado__numero--4")
+const side5 = document.querySelector(".dado__numero--5")
+const side6 = document.querySelector(".dado__numero--6")
+
+const dado = document.querySelector("dado__numero")
+const btnRandom = document.querySelector(".gerar-dado")
+
+// Fazer lógica do dado
+
+// Caso seja true adicione um innerHtml
+
+// Fazer a lógica dos numero aleatório
