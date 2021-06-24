b0b484b36af823dfdd60f4862af02067f10f476f
Guilherme Ribeiro <augusto.gui.2208@gmail.com>
2021-06-23 21:37:03 -0300

    variaveis com produtos

---
+++ 8- carrinho-de-compra/app.js
@@ -0,0 +1,20 @@
+const products = [
+   {
+      id: 1,
+      name: "Teclado",
+      img: "https://images.pexels.com/photos/841228/pexels-photo-841228.jpeg?cs=srgb&dl=pexels-karol-d-841228.jpg&fm=jpg",
+      about: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio necessitatibus veniam iusto reiciendis eius ex sed facere esse, quisquam eos.",
+   },
+   {
+      id: 2,
+      name: "Fone",
+      img: "https://images.pexels.com/photos/1037999/pexels-photo-1037999.jpeg?cs=srgb&dl=pexels-moose-photos-1037999.jpg&fm=jpg",
+      about: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio necessitatibus veniam iusto reiciendis eius ex sed facere esse, quisquam eos.",
+   },
+   {
+      id: 3,
+      name: "Monitor",
+      img: "https://images.pexels.com/photos/1714202/pexels-photo-1714202.jpeg?cs=srgb&dl=pexels-josh-sorenson-1714202.jpg&fm=jpg",
+      about: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio necessitatibus veniam iusto reiciendis eius ex sed facere esse, quisquam eos.",
+   },
+]
