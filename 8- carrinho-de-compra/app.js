3c031ec69bbef2a348334efd401d24a2d3316cd1
Guilherme Ribeiro <augusto.gui.2208@gmail.com>
2021-06-25 00:05:44 -0300

    Adicionando no carrinho

--- 8- carrinho-de-compra/app.js
+++ 8- carrinho-de-compra/app.js
@@ -26,7 +26,9 @@ const productInfo = document.querySelector(".buys__info--p");
 const productNext = document.querySelector(".buys__btns--next");
 const productPrev = document.querySelector(".buys__btns--prev");
 
-const openCar = document.querySelector("buys__btns--add");
+const addInCar = document.querySelector(".buys__btns--add");
+const openModal = document.querySelector(".open-modal");
+const modaToggle = document.querySelector(".modal");
 
 let currentItem = 0;
 
@@ -52,11 +54,57 @@ productNext.addEventListener("click", () => {
 productPrev.addEventListener("click", () => {
   currentItem--;
   if (currentItem < 0) {
-    currentItem = 0
+    currentItem = 0;
   }
   productShow(currentItem);
 });
 
+function addShop() {
+  const carItem = [];
+  const carModal = document.querySelector(".car-modal");
+  const containerModal = document.createElement("div");
+  // containerModal.classList.add("products");
+
+  const addItems = () => {
+    for (let i = 0; i < products.length - 2; i++)
+      carItem.push(products[currentItem]);
+    containerModal.innerHTML += `
+      <div class="products">
+        <div class="products__img">
+          <img
+            src="https://images.pexels.com/photos/841228/pexels-photo-841228.jpeg?cs=srgb&dl=pexels-karol-d-841228.jpg&fm=jpg"
+            alt="Foto do produto"
+          />
+        </div>
+        <div class="products__info">
+          <span class="products__info--span"> Teclado </span>
+        </div>
+        <div class="products__bnt">
+          <div class="products__btn--unidade">
+            <span>1</span>
+          </div>
+          <div class="products__btn">
+            <button class="products__bnt--remove">
+              <i class="far fa-minus-square"></i>
+            </button>
+            <button class="products__bnt--add">
+              <i class="far fa-plus-square"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+        `;
+    carModal.appendChild(containerModal);
+    console.log(carItem);
+  };
+
+  addInCar.addEventListener("click", addItems);
+
+  openModal.addEventListener("click", () => {
+    modaToggle.classList.toggle("active");
+  });
+}
+addShop();
 
 // Montar o modal
 // Criar lógica para mostrar o modal ao clicar no carrinho.
