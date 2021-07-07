b35b7d646115a0225ef55b296cd8decf5591b820
Guilherme Ribeiro <augusto.gui.2208@gmail.com>
2021-07-07 15:28:28 -0300

    finalizado

--- 8- api-pokedex/app.js
+++ 8- api-pokedex/app.js
@@ -1,5 +1,25 @@
+alert("API fornecida pela 'https://pokeapi.co/docs/v2'");
 const main = document.querySelector("main");
-const pokemon_number = 36;
+let pokemon_number = 130;
+
+// Códigos das cores pegada do canal "Florin Pop" 1/2
+const colors = {
+  fire: "#FDDFDF",
+  grass: "#DEFDE0",
+  electric: "#FCF7DE",
+  water: "#DEF3FD",
+  ground: "#f4e7da",
+  rock: "#d5d5d4",
+  fairy: "#fceaff",
+  poison: "#98d7a5",
+  bug: "#f8d5a3",
+  dragon: "#97b3e6",
+  psychic: "#eaeda1",
+  flying: "#F5F5F5",
+  fighting: "#E6E0D4",
+  normal: "#F5F5F5",
+};
+const main_types = Object.keys(colors);
 
 const pokemonGenerator = async () => {
   for (let i = 1; i <= pokemon_number; i++) {
@@ -21,16 +41,23 @@ pokemonGenerator();
 
 function createCardPokemon(pokemons) {
   const { name, id, types } = pokemons;
-  console.log(id);
+
+  // Códigos das cores pegada do canal "Florin Pop" 2/2
+  const poke_types = pokemons.types.map((type) => type.type.name);
+  const type = main_types.find((type) => poke_types.indexOf(type) > -1);
+  const color = colors[type];
 
   const cardPokemon = document.createElement("div");
   cardPokemon.classList.add("card");
+
+  cardPokemon.style.backgroundColor = color;
+
   cardPokemon.innerHTML = `
     <div class="card__img">
        <img src="https://pokeres.bastionbot.org/images/pokemon/${id}.png" alt="">
      </div>
      <div class="card__id">
-       <span>#${id}</span>
+       <span>#${id.toString().padStart(3, "0")}</span>
      </div>
      <div class="card__name">
        <span>${name}</span>
