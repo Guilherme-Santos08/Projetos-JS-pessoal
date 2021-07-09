0812fe94ba0e7cbba332a0e19e423d3c825b6509
Guilherme Ribeiro <augusto.gui.2208@gmail.com>
2021-07-08 21:46:58 -0300

    API antiga

--- 9- app-music/app.js
+++ 9- app-music/app.js
@@ -1,25 +1,25 @@
 const searchCity = document.getElementById("search")
 
 async function getWeather(e) {
-   const API_URL = `https://api.troposphere.io/place/name/${e}?token=bbce94d2cfc23aefd03a7346b49d6ae929c57ecc2644e3df2c`
-   const res = await fetch(API_URL)
-   const data = await res.json()
+   const API_PLACE = `https://api.troposphere.io/place/name/${e}?token=bbce94d2cfc23aefd03a7346b49d6ae929c57ecc2644e3df2c`
+   const resPlace = await fetch(API_PLACE)
+   const dataPlace = await resPlace.json()
 
-   const teste = (data.data[0])
+   const teste = (dataPlace.data[0])
    const {latitude, longitude} = teste
 
-   const API_URL2 = `https://api.troposphere.io/forecast/${latitude},${longitude}?token=bbce94d2cfc23aefd03a7346b49d6ae929c57ecc2644e3df2c`
-   const res2 = await fetch(API_URL2)
-   const data2 = await res2.json()
+   const API_FORECAST = `https://api.troposphere.io/forecast/${latitude},${longitude}?token=bbce94d2cfc23aefd03a7346b49d6ae929c57ecc2644e3df2c`
+   const resForecast = await fetch(API_FORECAST)
+   const dataForecast = await resForecast.json()
 
-   console.log(data2.data)
-   console.log(data2.data.current.temperature)
+   console.log(dataForecast.data)
+   console.log(dataForecast.data.current.temperature)
 }
 
 getWeather("São Paulo")
 
 // teste(-23.5475,-46.63611)
 
-searchCity.addEventListener("input", (e) => {
-   console.log(e.target.value)
-})
+// searchCity.addEventListener("input", (e) => {
+//    console.log(e.target.value)
+// })
